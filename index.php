f3d79328cb0703239ecaacfe9fa59016e6adac25
TheSunshine <sunshineduded@gmail.com>
2019-07-07 12:26:39 +0200

    v0.0.0

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+    <link rel="stylesheet" href="CSS/AllFather.css" type="text/css">
+    <title>MMO.sy - Najlepszy portal o MMO</title>
+    <meta name="description" content="Strona poświęcona tematowi gier z gatunku MMO - Massively multiplayer online. Na stronie głównie ukazują się najnowsze wiadomości oraz porównania, lecz również nie brakuje ciekawostek.">
+
+</head>
+<script>
+function menubutton (){
+    NavContainer.classList.toggle("NavContainerToggle"); 
+    NavButton.classList.toggle("ButtonToggle"); 
+}    
+</script>
+<body>
+    <nav>
+        <div id="NavLeft">
+            <img src="CSS/SVG/Bug.svg">
+            <img src="CSS/SVG/Log.svg">
+        </div>
+        <div id="NavRight">
+            <div id="Contact">Kontakt</div>
+        </div>
+        <div class="NavContainer">
+        <div class="NavBox">
+            <div id="NavLogo"><a href="index.php">MMO.SY</a></div>
+            <div onclick="NavButton()" id="NavButton" class="NavButtonStyle">
+                <img src="CSS/SVG/menu-button.svg">
+            </div>
+        </div>
+        <ul id="NavContainer">
+            <li>Newsy</li>
+            <ul class="NavInNav">
+                <li>TOP ALL</li>
+                <li>TOP NOWE</li>
+            </ul>
+            <li>Ranking</li>
+        </ul>
+        </div>
+    </nav>
+    
+    <footer>
+        <div class="Box">
+            <div class="Container">
+               <span class="Info">
+                MMO.SY to serwis informacyjny o nowinkach w świecie gier,
+        rankingach, prównaniach oraz spora baza informacji na temat MMO.<br><br>
+
+        Strona została założona w 2019 roku i nadal się rozwija.<br>
+<br>
+        Projekt nadal jest w fazie budowy, więc jeśli chcesz zgłosić buga, 
+        to możesz to zrobić klikając odpowiedni przycisk w menu lub poniżej.
+                </span>
+            </div>
+        </div>
+        <div class="Box">
+            <div class="Container">
+                <div class="Container2">
+               <span class="Info">
+                Copyright 2019 © / TheSunshine / All rights reservedsaaaaaaaaadsaaaaaaafdsfdsaaaaadsssssssaaa.
+                </span>
+                </div>
+                <div class="ButtonBox">
+                    <div class="BugBox">
+                        <span>BUG</span> 
+                        <img src="CSS/SVG/Bug.svg">
+                    </div>
+                </div>
+                <div class="ButtonBox">
+                    <div class="BugBox">
+                        <img src="CSS/SVG/Log.svg">
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </footer>
+</body>
+
+</html>
